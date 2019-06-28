25089357f7a0847794d93cb067ed385231accc19
zzyybs <zzyybs@126.com>
2019-06-28 23:37:08 +0800

    new project

---
+++ src/main/java/com/atguigu/github/HelloGitHub.java
@@ -0,0 +1,13 @@
+package com.atguigu.github;
+
+/**
+ * @auther zzyy
+ * @create 2019-06-28 23:21
+ */
+public class HelloGitHub
+{
+    public static void main(String[] args)
+    {
+        System.out.println("我是github测试工程，我在github上");
+    }
+}
