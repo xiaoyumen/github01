a16c1e80dd2337e8ad1275e77a36050f4b4ff5e7
zzyybs <zzyybs@126.com>
2019-06-29 15:50:40 +0800

    mysql003

--- src/main/java/com/atguigu/github/HelloGitHub.java
+++ src/main/java/com/atguigu/github/HelloGitHub.java
@@ -9,6 +9,6 @@ public class HelloGitHub
     public static void main(String[] args)
     {
         System.out.println("我是github测试工程，我在github上");
-        System.out.println("mysql002");
+        System.out.println("mysql003");
     }
 }
