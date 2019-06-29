3bfe06819f1ea71f28b60190d71cebb5714b92fe
zzyybs <zzyybs@126.com>
2019-06-29 16:04:42 +0800

    del
    del

--- src/main/java/com/atguigu/github/HelloGitHub.java
+++ src/main/java/com/atguigu/github/HelloGitHub.java
@@ -9,9 +9,5 @@ public class HelloGitHub
     public static void main(String[] args)
     {
         System.out.println("我是github测试工程，我在github上");
-        System.out.println("mysql003");
-        System.out.println("java001");
-        System.out.println("bbbbb");
-        System.out.println("aaaaaaa");
     }
 }
