64e741ef96c8f37fef27cb215936ff89e657a8ac
zzyybs <zzyybs@126.com>
2019-07-04 15:29:42 +0800

    clear
    clear

--- src/main/java/com/atguigu/github/HelloGitHub.java
+++ src/main/java/com/atguigu/github/HelloGitHub.java
@@ -9,10 +9,5 @@ public class HelloGitHub
     public static void main(String[] args)
     {
         System.out.println("我是github测试工程，我在github上");
-        System.out.println("AAAAAA");
-        System.out.println("333333");
-        System.out.println("BBBBBB");
-        System.out.println("xxxxxx");
-
     }
 }
